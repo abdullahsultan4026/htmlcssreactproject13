49c19e2df9f3db5c0ddcc1e54b92c67128967803
abdullahsultan4026 <abdullahsultan4026@gmail.com>
2026-03-31 02:42:04 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,72 @@
+export default function Header() {
+  return (
+    <>
+      <div class="container">
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+        <div></div>
+        <div class="blacks"></div>
+      </div>
+    </>
+  );
+}
